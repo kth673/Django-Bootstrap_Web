63d5b16f3c49d79d09920247b131a020b8d7e53f
deny <kth673@naver.com>
2024-08-17 23:19:09 +0900

    Post 모델 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class POST(models.Model):
+  title = models.CharField(max_length=30)
+  content = models.TextField()
+  
+  created_at = models.DateTimeField()
+  # athor : 추후 작성 예정
