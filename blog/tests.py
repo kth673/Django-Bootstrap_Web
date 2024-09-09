4615224290f8f291818f2820dfe5b07b69b4feb6
deny <kth673@naver.com>
2024-09-09 22:07:01 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -82,8 +82,9 @@ class TestView(TestCase):
     self.assertIn(self.post_002.category.name, post_002_card.text)
     
     post_003_card = main_area.find('div', id='post-3')
-    self.assertIn('미분류', post_003_card.text)
-    self.assertIn(self.post_003.category.name, post_003_card.text)
+    self.assertIn('미분류', post_003_card.text)  # post_003의 카테고리가 없으므로 미분류 처리
+    self.assertIn(self.post_003.title, post_003_card.text)
+
 
     self.assertIn(self.user_trump.username.upper(), main_area.text)
     self.assertIn(self.user_obama.username.upper(), main_area.text)
@@ -100,35 +101,39 @@ class TestView(TestCase):
 
   def test_post_detail(self):
     # 1.1. 포스트가 하나 있다.
-    post_001 = Post.objects.create(
-      title='첫 번째 포스트입니다.',
-      content='Hello World. We are the world.',
-      author=self.user_trump
+    # post_001 = Post.objects.create(
+    #   title='첫 번째 포스트입니다.',
+    #   content='Hello World. We are the world.',
+    #   author=self.user_trump
 
-    )
+    # )
+    
     # 1.2. 포스트의 url은 '/Blog/1/'이다.
-    self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+    self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
     
     
     # 2. 첫 번째 포스트의 상세 페이지 테스트
     # 2.1. 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다(status code:200).
-    response = self.client.get(post_001.get_absolute_url())
+    response = self.client.get(self.post_001.get_absolute_url())
     self.assertEqual(response.status_code, 200)
     soup = BeautifulSoup(response.content, 'html.parser')
     
     # 2.2. 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
     self.navbar_test(soup)
     
+    self.category_card_test(soup)
+    
     # 2.3. 첫 번째 포스트의 제목이 웹 브라우저 탭 타이블에 들어 있다.
-    self.assertIn(post_001.title, soup.title.text)
+    self.assertIn(self.post_001.title, soup.title.text)
     
     # 2.4. 첫 번째 포스트의 제목이 포스트 영역에 있다.
     main_area = soup.find('div', id='main-area')
     post_area = main_area.find('div', id='post-area')
-    self.assertIn(post_001.title, post_area.text)
+    self.assertIn(self.post_001.title, post_area.text)
+    self.assertIn(self.category_programming.name, post_area.text)
     
     # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역(post-area)에 있다.
     self.assertIn(self.user_trump.username.upper(), post_area.text)
     
     # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
-    self.assertIn(post_001.content, post_area.text)
+    self.assertIn(self.post_001.content, post_area.text)
